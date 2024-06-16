b575d136a97b6ecfc9439cec0f2716916abda808
Paradis <Lucasezequiel120202@gmail.com>
2024-06-16 06:09:23 -0300

    fix env missing

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['lh3.googleusercontent.com'],
+    },
+    env: {
+        NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    },
+};
 
 export default nextConfig;
