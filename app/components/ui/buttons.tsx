79831f704e1a59f5642f67cf1e8dc61d594c2378
Zeta <ag.sebastian912@gmail.com>
2024-07-16 21:24:11 -0300

    logged home CSS

--- app/components/ui/buttons.tsx
+++ app/components/ui/buttons.tsx
@@ -1,11 +1,18 @@
-
 "use client"
 import { ButtonProps } from "../../types";
 import { useRouter } from "next/navigation";
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 
-const Buttons = ({text, containerStyle, navigateTo }: ButtonProps) => {
+
+const Buttons = ({text, containerStyle, navigateTo, icon }: ButtonProps) => {
     const router = useRouter();
+    const [hasMounted, setHasMounted] = useState(false);
+
+    useEffect(() => {
+        setHasMounted(true);
+    }, []);
 
     const onClick = ( ) => {
         if (navigateTo) {
@@ -17,7 +24,12 @@ const Buttons = ({text, containerStyle, navigateTo }: ButtonProps) => {
     };
 
     return (
-        <button className={containerStyle} onClick={onClick}>
+        <button className={`${containerStyle} flex items-center`} onClick={onClick}>
+            {icon ? (
+                <div className={`mr-2 inline-block ${!hasMounted ? 'hidden' : ''}`}>
+                    <Image src={icon} alt="button icon" width={20} height={20} />
+                </div>
+            ) : null}
             {text}
         </button>
     );
--- app/components/ui/productor.tsx
+++ app/components/ui/productor.tsx
@@ -10,16 +10,16 @@ const Productor = () => {
     return (
     //Container
     <div className="w-full min-h-[400px] max-h-[995px] relative content-center bg-cover mb-[150px] mt-[150px]">
-      <Image className="z-0 relative h-[50vh] md:max-h-[615px] bg-cover" src={imgProd} alt="" />
+      <Image className="z-0 relative h-[615px] md:max-h-[615px] bg-cover" src={imgProd} alt="" />
       <div className="absolute inset-0 bg-black opacity-75"></div>
-      <div className='absolute inset-0 flex flex-col justify-center items-center text-center h-[25vh] w-[312px] md:max-w-[1098px] md:max-h-[412px] border-[2px] border-solid bg-opacity-10 bg-white mx-auto my-auto rounded-2xl'>
-        <div className="py-[4px] px-[8px] md:py-[8px] md:px-4 bg-[#292929] text-white rounded md:rounded-2xl text-[10px] md:text-xl font-normal border-solid border-white border-1 mb-[16px] mx-auto flex">
+      <div className='absolute inset-0 flex flex-col justify-center items-center text-center h-[25vh] w-[312px] md:max-w-[1098px] md:h-[412px] md:w-[1060px] border-[2px] border-solid bg-opacity-10 bg-white mx-auto my-auto rounded-2xl'>
+        <div className="py-[4px] px-[8px] md:py-[8px] md:px-4 bg-[#292929] text-white rounded md:rounded-3xl text-[10px] md:text-xl font-normal border-solid border-white border-1 mb-[16px] mx-auto flex">
                 <Image className="w-[15px] md:w-[25px] mr-[10px]" src={logoG} alt=""/>
                 Trabajemos juntos</div>
-        <h1 className='opacity-100 z-4 font-semibold text-xl md:text-[52px] text-white flex flex-col justify-center md:mt-3 leading-[26px]'>¿Sos Productor de Eventos?<br/><span className='text-base font-semibold leading-6'>¡Produce tus eventos con nosotros!</span></h1>
+        <h1 className='z-4 font-semibold text-xl md:text-[42px] text-white flex flex-col justify-center md:mb-4 md:mt-3 md:leading-[54px]'>¿Sos Productor de Eventos?<br/><span className='text-xl md:text-[42px] font-semibold leading-6 md:leading-[54px]'>¡Produce tus eventos con nosotros!</span></h1>
         <Buttons 
                  text="Registrarse Como Productor"
-                 containerStyle="bg-white text-black rounded md:rounded-xl mt-3 py-[11px] px-[16px] md:py-[20px] md:px-[36px] text-[9px] font-normal tracking-wide gap-[10px]"
+                 containerStyle="bg-white text-black rounded md:rounded-xl mt-3 py-[11px] px-[16px] md:py-[20px] md:px-[36px] text-[9px] font-normal md:text-base md:font-semibold tracking-wide gap-[10px]"
                 />
       </div>
       </div>
