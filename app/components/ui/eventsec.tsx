3210ead0bfd8f7372296ee3823786682140da73f
Zeta <ag.sebastian912@gmail.com>
2024-07-23 22:49:30 -0300

    Update eventsec.tsx

--- app/components/ui/eventsec.tsx
+++ app/components/ui/eventsec.tsx
@@ -1,9 +1,29 @@
+"use client"
 import React from 'react'
 import Image from 'next/image';
 import isoCol from "@/assets/isoColW.png";
+import { useEffect, useState } from 'react';
+import { readEvents } from '@/models/queries/events';
+
+type Events = {
+  id: string;
+  name: string;
+};
 
 
 const Eventsec = () => {
+
+  const [events, setEvents] = useState<Events[]>([]);
+  const limit = 9;
+
+  useEffect(() => {
+    async function readEvents() {
+      const response = await fetch(`/api/events?limit=${limit}`);
+      const data = await response.json();
+      setEvents(data);
+    }
+    readEvents();
+  }, [limit]);
 
     return (
         <div className="max-w-[1440px] flex justify-center relative mx-auto">
@@ -16,7 +36,19 @@ const Eventsec = () => {
                 <h1 className="text-black text-5xl font-koulen mt-5 mx-auto">descubre los eventos disponibles</h1>
                 <p className="text-gray-400 text-base font-normal mt-3 mx-auto">Busca todos los eventos que existen en
                     nuestra plataforma.</p>
+
+                    <div className="max-w-4xl mx-auto p-4">
+            <h1 className="text-2xl font-bold mb-4">Your Events</h1>
+            <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+             {events.map(event => (
+            <li key={event.id} className="bg-white rounded-lg shadow p-4">
+            <h2 className="text-lg font-semibold">{event.name}</h2>
+          </li>
+        ))}
+      </ul>
+    </div>
             </div>
+            
         </div>
     )
 }
